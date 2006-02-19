b3a1030e980a58739346bcbca3a670db1184f707
agata <agata@a0133a49-3802-0410-849b-833ed23e894f>
2006-02-19 16:38:53 +0000

    change refactring
    git-svn-id: https://www.seasar.org/svn/s2dao/trunk/s2dao@212 a0133a49-3802-0410-849b-833ed23e894f

--- s2-dao/src/main/java/org/seasar/dao/pager/PagerResultSetFactoryLimitOffsetWrapper.java
+++ s2-dao/src/main/java/org/seasar/dao/pager/PagerResultSetFactoryLimitOffsetWrapper.java
@@ -22,9 +22,12 @@ import java.sql.SQLException;
 import org.seasar.extension.jdbc.ResultSetFactory;
 import org.seasar.framework.exception.SQLRuntimeException;
 import org.seasar.framework.log.Logger;
+import org.seasar.framework.util.ResultSetUtil;
+import org.seasar.framework.util.StatementUtil;
 
 /**
  * @author yamamoto
+ * @author agata
  */
 public class PagerResultSetFactoryLimitOffsetWrapper implements
         ResultSetFactory {
@@ -62,53 +65,81 @@ public class PagerResultSetFactoryLimitOffsetWrapper implements
         Object[] args = PagerContext.getContext().peekArgs();
 
         if (PagerContext.isPagerCondition(args)) {
-
             try {
-                String baseSQL = ps.toString().replaceFirst("^.*SELECT",
-                        "SELECT");
-                StringBuffer sqlBuf = new StringBuffer("SELECT count(*) FROM (");
-                sqlBuf.append(baseSQL);
-                sqlBuf.append(") AS total");
+				String baseSQL = makeBaseSql(ps.toString());
+                PagerCondition dto = PagerContext.getPagerCondition(args);
+				dto.setCount(getCount(ps, baseSQL));
+				if (dto.getLimit() > 0 && dto.getOffset() > -1) {
+					String limitOffsetSql = 
+                		makeLimitOffsetSql(baseSQL, dto.getLimit(), dto.getOffset());
+					LOGGER.debug("S2Pager execute SQL : " + limitOffsetSql);
 
-                LOGGER.debug("S2Pager execute SQL : " + sqlBuf.toString());
-
-                PreparedStatement psCount = ps.getConnection()
-                        .prepareStatement(sqlBuf.toString());
-                ResultSet rs = resultSetFactory_.createResultSet(psCount);
-
-                if (rs.next()) {
-                    PagerCondition dto = PagerContext.getPagerCondition(args);
-                    dto.setCount(rs.getInt(1));
-
-                    if (dto.getLimit() > 0 && dto.getOffset() > -1) {
-                        sqlBuf = new StringBuffer(baseSQL);
-                        sqlBuf.append(" LIMIT ");
-                        sqlBuf.append(dto.getLimit());
-                        sqlBuf.append(" OFFSET ");
-                        sqlBuf.append(dto.getOffset());
-
-                        LOGGER.debug("S2Pager execute SQL : "
-                                + sqlBuf.toString());
-
-                        rs = resultSetFactory_.createResultSet(ps
-                                .getConnection().prepareStatement(
-                                        sqlBuf.toString()));
-
-                    } else {
-                        rs = resultSetFactory_.createResultSet(ps);
-                    }
-                    return rs;
+                    return resultSetFactory_.createResultSet(ps
+                            .getConnection().prepareStatement(
+                                    limitOffsetSql));
                 } else {
-                    throw new SQLException();
+                    return resultSetFactory_.createResultSet(ps);
                 }
-
             } catch (SQLException e) {
                 throw new SQLRuntimeException(e);
             }
-
         } else {
             return resultSetFactory_.createResultSet(ps);
         }
     }
+    /**
+     * limit offsetを付加したSQLを作成します。
+     * @param baseSQL
+     * @param limit
+     * @param offset
+     * @return
+     */
+	String makeLimitOffsetSql(String baseSQL, int limit, int offset) {
+		StringBuffer sqlBuf = new StringBuffer(baseSQL);
+		sqlBuf.append(" LIMIT ");
+		sqlBuf.append(limit);
+		sqlBuf.append(" OFFSET ");
+		sqlBuf.append(offset);
+		return sqlBuf.toString();
+	}
+    /**
+     * JDBCドライバによっては、selectの前に余計な文字列が
+     * 付加されるためselectの前を除去してオリジナルのSQLを取得します。
+     * @param nativeSql JDBCドライバ固有のSQL
+     * @return オリジナルのSQL
+     */
+	String makeBaseSql(String nativeSql) {
+		return nativeSql.replaceFirst("^.*SELECT",
+		        "SELECT");
+	}
+    /**
+     * 元のSQLによる結果総件数を取得します
+     * @param ps 元のPreparedStatement
+     * @param baseSQL 元のSQL
+     * @return 結果総件数
+     * @throws SQLException
+     */
+	private int getCount(PreparedStatement ps, String baseSQL) throws SQLException {
+		StringBuffer sqlBuf = new StringBuffer("SELECT count(*) FROM (");
+		sqlBuf.append(baseSQL);
+		sqlBuf.append(") AS total");
+		LOGGER.debug("S2Pager execute SQL : " + sqlBuf.toString());
+
+		PreparedStatement psCount = null;
+		ResultSet rs = null;
+		try {
+			psCount = ps.getConnection()
+	        .prepareStatement(sqlBuf.toString());
+			rs = resultSetFactory_.createResultSet(psCount);
+			if (rs.next()) {
+			    return rs.getInt(1);
+			} else {
+			    throw new SQLException("[S2Pager]Result not found.");
+			}
+		} finally {
+			ResultSetUtil.close(rs);
+			StatementUtil.close(psCount);
+		}
+	}
 
 }
