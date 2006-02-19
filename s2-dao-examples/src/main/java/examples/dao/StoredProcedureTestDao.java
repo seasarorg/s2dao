d6811b8dc75122e33e072548fa14815f0f443c9a
keizou <keizou@a0133a49-3802-0410-849b-833ed23e894f>
2006-02-19 19:39:03 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao/trunk/s2dao@224 a0133a49-3802-0410-849b-833ed23e894f

--- s2-dao-examples/src/main/java/examples/dao/StoredProcedureTestDao.java
+++ s2-dao-examples/src/main/java/examples/dao/StoredProcedureTestDao.java
@@ -21,12 +21,12 @@ import java.util.Map;
 
 public interface StoredProcedureTestDao {
 	public Class BEAN = Employee.class;
-	public String getSalesTax_PROCEDURE = "sales_tax";
+	public String getSalesTax_PROCEDURE = "SALES_TAX";
 	public double getSalesTax(double subtotal);
-	public String getSalesTax2_PROCEDURE = "sales_tax2";
+	public String getSalesTax2_PROCEDURE = "SALES_TAX2";
 	public double getSalesTax2(double subtotal);
-	public String getSalesTax3_PROCEDURE = "sales_tax3";
+	public String getSalesTax3_PROCEDURE = "SALES_TAX3";
 	public double getSalesTax3(double subtotal);
-	public String getSalesTax4_PROCEDURE = "sales_tax4";
+	public String getSalesTax4_PROCEDURE = "SALES_TAX4";
 	public Map getSalesTax4(double subtotal);
 }
