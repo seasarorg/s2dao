5b7fab237df953edcecab2acca1e86cc379bba54
agata <agata@a0133a49-3802-0410-849b-833ed23e894f>
2006-02-19 16:39:13 +0000

    add test
    git-svn-id: https://www.seasar.org/svn/s2dao/trunk/s2dao@213 a0133a49-3802-0410-849b-833ed23e894f

---
+++ s2-dao/src/test/java/org/seasar/dao/pager/PagerResultSetFactoryLimitOffsetWapperTest.java
@@ -0,0 +1,49 @@
+package org.seasar.dao.pager;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Toshitaka Agata
+ */
+public class PagerResultSetFactoryLimitOffsetWapperTest extends TestCase {
+    
+    MockResultSetFactory original;
+    PagerResultSetFactoryLimitOffsetWrapper wrapper;
+    
+    protected void setUp() throws Exception {
+        original = new MockResultSetFactory();
+        wrapper = new PagerResultSetFactoryLimitOffsetWrapper(original);
+    }
+    
+    public void testMakeBaseSql() throws Exception {
+        try {
+            PagerContext.getContext().pushArgs(createNormalArgs());
+            assertEquals(
+            		"SELECTの前のネイティブSQLを除去",
+            		"SELECT * FROM DEPARTMENT", 
+            		wrapper.makeBaseSql("native sql ... SELECT * FROM DEPARTMENT"));
+            assertEquals(
+            		"ネイティブSQLが存在しない場合、元のSQLも変化なし",
+            		"SELECT * FROM DEPARTMENT", 
+            		wrapper.makeBaseSql("SELECT * FROM DEPARTMENT"));
+        } finally {
+            PagerContext.getContext().popArgs();
+        }
+
+    }
+    public void testLimitOffsetSql() throws Exception {
+        try {
+            PagerContext.getContext().pushArgs(createNormalArgs());
+            assertEquals(
+            		"指定されたlimit offsetが付加されたSQLを生成",
+            		"SELECT * FROM DEPARTMENT LIMIT 10 OFFSET 55", 
+            		wrapper.makeLimitOffsetSql("SELECT * FROM DEPARTMENT", 10, 55));
+        } finally {
+            PagerContext.getContext().popArgs();
+        }
+
+    }
+    private Object[] createNormalArgs() {
+        return new Object[] {};
+    }
+}
