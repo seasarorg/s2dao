1b21592574e1949f9e0e895d9b27ef3ca266d25c
agata <agata@a0133a49-3802-0410-849b-833ed23e894f>
2006-02-22 12:59:59 +0000

    fixed bug PageUtil#getCurrentLastOffset
    git-svn-id: https://www.seasar.org/svn/s2dao/trunk/s2dao@232 a0133a49-3802-0410-849b-833ed23e894f

--- s2-dao/src/test/java/org/seasar/dao/pager/PagerConditionBaseTest.java
+++ s2-dao/src/test/java/org/seasar/dao/pager/PagerConditionBaseTest.java
@@ -30,17 +30,17 @@ public class PagerConditionBaseTest extends TestCase {
 
     public void testFirstPage() {
         setCondtion(0, 10, 95);
-        assertCondtion(false, true, 0, 1, 0, 10, 10, 9);
+        assertCondtion(false, true, 0, 1, 0, 10, 9, 9);
     }
 
     public void testSecondPage() {
         setCondtion(10, 10, 95);
-        assertCondtion(true, true, 1, 2, 0, 20, 20, 9);
+        assertCondtion(true, true, 1, 2, 0, 20, 19, 9);
     }
 
     public void testLastPage() {
         setCondtion(90, 10, 95);
-        assertCondtion(true, false, 9, 10, 80, 100, 95, 9);
+        assertCondtion(true, false, 9, 10, 80, 100, 94, 9);
     }
 
     public void testEmptyResult() {
@@ -50,12 +50,12 @@ public class PagerConditionBaseTest extends TestCase {
 
     public void test9Result() {
         setCondtion(0, 10, 9);
-        assertCondtion(false, false, 0, 1, 0, 10, 9, 0);
+        assertCondtion(false, false, 0, 1, 0, 10, 8, 0);
     }
 
     public void test10Result() {
         setCondtion(0, 10, 10);
-        assertCondtion(false, false, 0, 1, 0, 10, 10, 0);
+        assertCondtion(false, false, 0, 1, 0, 10, 9, 0);
     }
 
     private void assertCondtion(boolean isPrev, boolean isNext, int pageIndex,
