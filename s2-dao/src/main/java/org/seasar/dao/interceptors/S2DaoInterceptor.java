3d71c6088d0fa27dcaf01a2c4486110994d6a652
manhole <manholex@gmail.com>
2006-04-24 05:51:33 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao/trunk/s2dao@317 a0133a49-3802-0410-849b-833ed23e894f

--- s2-dao/src/main/java/org/seasar/dao/interceptors/S2DaoInterceptor.java
+++ s2-dao/src/main/java/org/seasar/dao/interceptors/S2DaoInterceptor.java
@@ -27,34 +27,37 @@ import org.seasar.framework.util.NumberConversionUtil;
 
 /**
  * @author higa
- *  
+ * 
  */
 public class S2DaoInterceptor extends AbstractInterceptor {
 
-	private DaoMetaDataFactory daoMetaDataFactory_;
+    private static final long serialVersionUID = 1L;
 
-	public S2DaoInterceptor(DaoMetaDataFactory daoMetaDataFactory) {
-		daoMetaDataFactory_ = daoMetaDataFactory;
-	}
+    private DaoMetaDataFactory daoMetaDataFactory_;
 
-	/**
-	 * @see org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation)
-	 */
-	public Object invoke(MethodInvocation invocation) throws Throwable {
-		Method method = invocation.getMethod();
-		if (!MethodUtil.isAbstract(method)) {
-			return invocation.proceed();
-		}
-		Class targetClass = getTargetClass(invocation);
-		DaoMetaData dmd = daoMetaDataFactory_.getDaoMetaData(targetClass);
-		SqlCommand cmd = dmd.getSqlCommand(method.getName());
-		Object ret = cmd.execute(invocation.getArguments());
-		Class retType = method.getReturnType();
-		if (retType.isPrimitive()) {
-			return NumberConversionUtil.convertPrimitiveWrapper(retType, ret);
-		} else if (Number.class.isAssignableFrom(retType)) {
-			return NumberConversionUtil.convertNumber(retType, ret);
-		}
-		return ret;
-	}
+    public S2DaoInterceptor(DaoMetaDataFactory daoMetaDataFactory) {
+        daoMetaDataFactory_ = daoMetaDataFactory;
+    }
+
+    /**
+     * @see org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation)
+     */
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        Method method = invocation.getMethod();
+        if (!MethodUtil.isAbstract(method)) {
+            return invocation.proceed();
+        }
+        Class targetClass = getTargetClass(invocation);
+        DaoMetaData dmd = daoMetaDataFactory_.getDaoMetaData(targetClass);
+        SqlCommand cmd = dmd.getSqlCommand(method.getName());
+        Object ret = cmd.execute(invocation.getArguments());
+        Class retType = method.getReturnType();
+        if (retType.isPrimitive()) {
+            return NumberConversionUtil.convertPrimitiveWrapper(retType, ret);
+        } else if (Number.class.isAssignableFrom(retType)) {
+            return NumberConversionUtil.convertNumber(retType, ret);
+        }
+        return ret;
+    }
+
 }
