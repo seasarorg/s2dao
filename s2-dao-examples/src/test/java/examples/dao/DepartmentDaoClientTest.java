9ff5c993d00340c1be07dd04995b0d469dd196ba
manhole <manholex@gmail.com>
2006-05-09 23:24:56 +0000

    fixed: s2dao-examples problem. [Seasar-user:3465]
    git-svn-id: https://www.seasar.org/svn/s2dao/trunk/s2dao@359 a0133a49-3802-0410-849b-833ed23e894f

--- s2-dao-examples/src/test/java/examples/dao/DepartmentDaoClientTest.java
+++ s2-dao-examples/src/test/java/examples/dao/DepartmentDaoClientTest.java
@@ -15,30 +15,12 @@
  */
 package examples.dao;
 
-import java.util.List;
+import junit.framework.TestCase;
 
-public interface EmployeeDao {
+public class DepartmentDaoClientTest extends TestCase {
 
-	public Class BEAN = Employee.class;
-	
-	public List getAllEmployees();
-	
-	public String getEmployee_ARGS = "empno";
-
-	public Employee getEmployee(int empno);
-	
-	public int getCount();
-	
-	public String getEmployeeByJobDeptno_ARGS = "job, deptno";
-	
-	public List getEmployeeByJobDeptno(String job, Integer deptno);
-	
-	public String getEmployeeByDeptno_ARGS = "deptno";
-	public String getEmployeeByDeptno_QUERY = "/*IF deptno != null*/deptno = /*deptno*/123\n"+
-			"-- ELSE 1=1\n"+
-			"/*END*/";
-	public List getEmployeeByDeptno(Integer deptno);
-
-	public int update(Employee employee);
+    public void testNoException() throws Exception {
+        DepartmentDaoClient.main(new String[] {});
+    }
 
 }
