14beea156bae01f8dd3ffeeee67eab4460b3093f
manhole <manholex@gmail.com>
2006-06-05 14:25:01 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao/trunk/s2dao@413 a0133a49-3802-0410-849b-833ed23e894f

--- s2-dao/src/test/java/org/seasar/dao/impl/Employee7Dao.java
+++ s2-dao/src/test/java/org/seasar/dao/impl/Employee7Dao.java
@@ -19,11 +19,12 @@ public interface Employee7Dao {
 
     public Class BEAN = Employee.class;
 
-    public static String getCount_hsql_SQL = "SELECT COUNT(*) FROM emp;";
+    public static String getCount_hsql_SQL = "SELECT COUNT(*) FROM emp";
 
     public int getCount();
 
-    public static String deleteEmployee_SQL = "DELETE FROM emp WHERE empno=?;";
+    public static String deleteEmployee_SQL = "DELETE FROM emp WHERE empno=?";
 
     public int deleteEmployee(int empno);
+
 }
