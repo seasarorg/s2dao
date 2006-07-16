5c4138c4b381f24bf35a63e99d9084bde707f171
manhole <manholex@gmail.com>
2006-07-16 16:41:47 +0000

    [DAO-22]
    git-svn-id: https://www.seasar.org/svn/s2dao/trunk/s2dao@473 a0133a49-3802-0410-849b-833ed23e894f

---
+++ s2-dao/src/main/java/org/seasar/dao/impl/AnnotationReaderFactoryImpl.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.dao.impl;
+
+import org.seasar.dao.AnnotationReaderFactory;
+import org.seasar.dao.BeanAnnotationReader;
+import org.seasar.dao.DaoAnnotationReader;
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.exception.ClassNotFoundRuntimeException;
+import org.seasar.framework.util.ClassUtil;
+
+/**
+ * @author manhole
+ */
+public class AnnotationReaderFactoryImpl implements AnnotationReaderFactory {
+
+    private static final String TIGER_ANNOTATION_READER_FACTORY = "org.seasar.dao.annotation.tiger.impl.AnnotationReaderFactoryImpl";
+
+    private AnnotationReaderFactory annotationReaderFactory;
+
+    public AnnotationReaderFactoryImpl() {
+        Class clazz = FieldAnnotationReaderFactory.class;
+        try {
+            clazz = ClassUtil.forName(TIGER_ANNOTATION_READER_FACTORY);
+        } catch (ClassNotFoundRuntimeException ignore) {
+        }
+        annotationReaderFactory = (AnnotationReaderFactory) ClassUtil
+                .newInstance(clazz);
+    }
+
+    public BeanAnnotationReader createBeanAnnotationReader(Class beanClass) {
+        return annotationReaderFactory.createBeanAnnotationReader(beanClass);
+    }
+
+    public DaoAnnotationReader createDaoAnnotationReader(BeanDesc daoBeanDesc) {
+        return annotationReaderFactory.createDaoAnnotationReader(daoBeanDesc);
+    }
+
+}
---
+++ s2-dao/src/test/java/org/seasar/dao/impl/AnnotationReaderFactoryImplTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.dao.impl;
+
+import junit.framework.TestCase;
+
+import org.seasar.dao.BeanAnnotationReader;
+import org.seasar.dao.DaoAnnotationReader;
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+
+/**
+ * @author manhole
+ */
+public class AnnotationReaderFactoryImplTest extends TestCase {
+
+    private FieldAnnotationReaderFactory fieldAnnotationReaderFactory;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        fieldAnnotationReaderFactory = new FieldAnnotationReaderFactory();
+    }
+
+    public void testBeanAnnotationReader() throws Exception {
+        // ## Arrange ##
+        AnnotationReaderFactoryImpl annotationReaderFactory = new AnnotationReaderFactoryImpl();
+        final Class beanClass = Employee.class;
+
+        // ## Act ##
+        final BeanAnnotationReader beanAnnotationReader = annotationReaderFactory
+                .createBeanAnnotationReader(beanClass);
+
+        // ## Assert ##
+        assertEquals(fieldAnnotationReaderFactory.createBeanAnnotationReader(
+                beanClass).getClass(), beanAnnotationReader.getClass());
+    }
+
+    public void testDaoAnnotationReader() throws Exception {
+        // ## Arrange ##
+        AnnotationReaderFactoryImpl annotationReaderFactory = new AnnotationReaderFactoryImpl();
+        final BeanDesc daoBeanDesc = BeanDescFactory
+                .getBeanDesc(EmployeeDao.class);
+
+        // ## Act ##
+        final DaoAnnotationReader daoAnnotationReader = annotationReaderFactory
+                .createDaoAnnotationReader(daoBeanDesc);
+
+        // ## Assert ##
+        assertEquals(fieldAnnotationReaderFactory.createDaoAnnotationReader(
+                daoBeanDesc).getClass(), daoAnnotationReader.getClass());
+    }
+
+}
