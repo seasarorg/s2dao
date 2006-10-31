8c79e316d4f0726e80719499730bb96f1863a561
taichi <ryushi@gmail.com>
2006-10-31 08:46:52 +0000

    https://www.seasar.org/issues/browse/DAO-16の為のユニットテストを追加。
    git-svn-id: https://www.seasar.org/svn/s2dao/trunk/s2dao@578 a0133a49-3802-0410-849b-833ed23e894f

---
+++ s2-dao/src/test/java/org/seasar/dao/impl/PkOnlyTableTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.dao.impl;
+
+import org.seasar.dao.DaoMetaData;
+import org.seasar.dao.SqlCommand;
+import org.seasar.dao.unit.S2DaoTestCase;
+
+/**
+ * @author taichi
+ * 
+ */
+public class PkOnlyTableTest extends S2DaoTestCase {
+
+    public PkOnlyTableTest() {
+        super();
+    }
+
+    public PkOnlyTableTest(String name) {
+        super(name);
+    }
+
+    public void setUp() {
+        include("j2ee.dicon");
+    }
+
+    /*
+     * https://www.seasar.org/issues/browse/DAO-16
+     */
+    public void testInsert() throws Exception {
+        DaoMetaData dmd = createDaoMetaData(PkOnlyTableDao.class);
+        SqlCommand cmd = dmd.getSqlCommand("insert");
+        PkOnlyTable data = new PkOnlyTable();
+        data.setAaa("value");
+        Integer i = (Integer) cmd.execute(new Object[] { data });
+        assertEquals(1, i.intValue());
+    }
+
+    public class PkOnlyTable {
+        public static final String TABLE = "PKONLYTABLE";
+
+        private String aaa;
+
+        public String getAaa() {
+            return aaa;
+        }
+
+        public void setAaa(String aaa) {
+            this.aaa = aaa;
+        }
+    }
+
+    public interface PkOnlyTableDao {
+        Class BEAN = PkOnlyTable.class;
+
+        int insert(PkOnlyTable data);
+    }
+}
