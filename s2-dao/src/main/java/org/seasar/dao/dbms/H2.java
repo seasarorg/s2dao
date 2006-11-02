56056de60c0da84d87953eeb33ecf7bf3c2acb8c
taichi <ryushi@gmail.com>
2006-11-02 11:00:46 +0000

    [seasar-s2dao-dev:22]のコードを追加。
    git-svn-id: https://www.seasar.org/svn/s2dao/branches/s2dao-1.0.x@598 a0133a49-3802-0410-849b-833ed23e894f

---
+++ s2-dao/src/main/java/org/seasar/dao/dbms/H2.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.dao.dbms;
+
+/**
+ * @author taichi
+ */
+public class H2 extends Standard {
+
+    /**
+     * @see org.seasar.dao.Dbms#getSuffix()
+     */
+    public String getSuffix() {
+        return "_h2";
+    }
+
+    public String getIdentitySelectString() {
+        return "CALL IDENTITY()";
+    }
+
+    public String getSequenceNextValString(String sequenceName) {
+        return "CALL NEXTVAL('" + sequenceName + "')";
+    }
+}
