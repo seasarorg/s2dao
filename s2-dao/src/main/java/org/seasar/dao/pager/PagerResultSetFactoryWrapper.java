2cf363a8f00fd0b000161e19b23fdb8e78fabebd
manhole <manholex@gmail.com>
2007-04-04 03:44:50 +0000

    isUseScrollCursorを追加
    git-svn-id: https://www.seasar.org/svn/s2dao/branches/s2dao-1.0.x@823 a0133a49-3802-0410-849b-833ed23e894f

--- s2-dao/src/main/java/org/seasar/dao/pager/PagerResultSetFactoryWrapper.java
+++ s2-dao/src/main/java/org/seasar/dao/pager/PagerResultSetFactoryWrapper.java
@@ -43,6 +43,10 @@ public class PagerResultSetFactoryWrapper implements ResultSetFactory {
         this.resultSetFactory = resultSetFactory;
     }
 
+    public boolean isUseScrollCursor() {
+        return useScrollCursor;
+    }
+
     /**
      * @param useScrollCursor
      */
