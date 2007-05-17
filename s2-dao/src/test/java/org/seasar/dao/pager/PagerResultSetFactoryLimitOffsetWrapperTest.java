c1bc82ab233b4051ea3f96ccdba1e8d1e093534f
manhole <manholex@gmail.com>
2007-05-17 23:53:01 +0000

    failしていたので修正しました
    git-svn-id: https://www.seasar.org/svn/s2dao/branches/s2dao-1.0.x@874 a0133a49-3802-0410-849b-833ed23e894f

--- s2-dao/src/test/java/org/seasar/dao/pager/PagerResultSetFactoryLimitOffsetWrapperTest.java
+++ s2-dao/src/test/java/org/seasar/dao/pager/PagerResultSetFactoryLimitOffsetWrapperTest.java
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.dao.pager;
 
 import junit.framework.TestCase;
@@ -13,15 +27,19 @@ public class PagerResultSetFactoryLimitOffsetWrapperTest extends TestCase {
     PagerResultSetFactoryLimitOffsetWrapper wrapper;
 
     protected void setUp() throws Exception {
+        super.setUp();
         original = new MockResultSetFactory();
         wrapper = new PagerResultSetFactoryLimitOffsetWrapper(original, "MySQL");
+        PagerContext.start();
         PagerContext.getContext().pushArgs(createNormalArgs());
     }
 
     protected void tearDown() throws Exception {
         PagerContext.getContext().popArgs();
+        PagerContext.end();
+        super.tearDown();
     }
-    
+
     /*
      * public void testMakeBaseSql() throws Exception { try {
      * PagerContext.getContext().pushArgs(createNormalArgs()); assertEquals(
@@ -34,24 +52,19 @@ public class PagerResultSetFactoryLimitOffsetWrapperTest extends TestCase {
     public void testLimitOffsetSql() throws Exception {
         assertEquals("指定されたlimit offsetが付加されたSQLを生成",
                 "SELECT * FROM DEPARTMENT LIMIT 10 OFFSET 55", wrapper
-                        .makeLimitOffsetSql("SELECT * FROM DEPARTMENT", 10,
-                                55));
+                        .makeLimitOffsetSql("SELECT * FROM DEPARTMENT", 10, 55));
     }
 
     public void testMakeCountSql() throws Exception {
         assertEquals("count(*)で全件数を取得するSQLを生成",
                 "SELECT count(*) FROM (SELECT * FROM DEPARTMENT) AS total",
                 wrapper.makeCountSql("SELECT * FROM DEPARTMENT"));
-        assertEquals(
-                "count(*)で全件数を取得するSQLを生成(order by 除去)",
+        assertEquals("count(*)で全件数を取得するSQLを生成(order by 除去)",
                 "SELECT count(*) FROM (SELECT * FROM DEPARTMENT ) AS total",
-                wrapper
-                        .makeCountSql("SELECT * FROM DEPARTMENT order by id"));
-        assertEquals(
-                "count(*)で全件数を取得するSQLを生成(ORDER BY 除去)",
+                wrapper.makeCountSql("SELECT * FROM DEPARTMENT order by id"));
+        assertEquals("count(*)で全件数を取得するSQLを生成(ORDER BY 除去)",
                 "SELECT count(*) FROM (SELECT * FROM DEPARTMENT ) AS total",
-                wrapper
-                        .makeCountSql("SELECT * FROM DEPARTMENT ORDER BY id"));
+                wrapper.makeCountSql("SELECT * FROM DEPARTMENT ORDER BY id"));
         assertEquals(
                 "count(*)で全件数を取得するSQLを生成(whitespace付きorder by 除去)",
                 "SELECT count(*) FROM (SELECT * FROM DEPARTMENT\n) AS total",
@@ -72,11 +85,9 @@ public class PagerResultSetFactoryLimitOffsetWrapperTest extends TestCase {
                 "SELECT count(*) FROM (SELECT * FROM DEPARTMENT WHERE\n--order by\nname=1\n) AS total",
                 wrapper
                         .makeCountSql("SELECT * FROM DEPARTMENT WHERE\n--order by\nname=1\norder by id"));
-        assertEquals(
-                "count(*)で全件数を取得するSQLを生成(order by除去 UNICODE)",
+        assertEquals("count(*)で全件数を取得するSQLを生成(order by除去 UNICODE)",
                 "SELECT count(*) FROM (SELECT * FROM DEPARTMENT ) AS total",
-                wrapper
-                        .makeCountSql("SELECT * FROM DEPARTMENT order by ＮＯ"));
+                wrapper.makeCountSql("SELECT * FROM DEPARTMENT order by ＮＯ"));
         assertEquals(
                 "count(*)で全件数を取得するSQLを生成(order by除去 UNICODE)",
                 "SELECT count(*) FROM (SELECT * FROM DEPARTMENT ) AS total",
@@ -95,20 +106,18 @@ public class PagerResultSetFactoryLimitOffsetWrapperTest extends TestCase {
     }
 
     public void testSetChopOrderByAndMakeCountSql() throws Exception {
-        assertEquals(
-                "count(*)で全件数を取得するSQLを生成(chopOrderBy=true, order by 除去)",
+        assertEquals("count(*)で全件数を取得するSQLを生成(chopOrderBy=true, order by 除去)",
                 "SELECT count(*) FROM (SELECT * FROM DEPARTMENT ) AS total",
-                wrapper
-                        .makeCountSql("SELECT * FROM DEPARTMENT order by id"));
+                wrapper.makeCountSql("SELECT * FROM DEPARTMENT order by id"));
         wrapper.setChopOrderBy(false);
         assertEquals(
                 "count(*)で全件数を取得するSQLを生成(chopOrderBy=false, order by 除去)",
                 "SELECT count(*) FROM (SELECT * FROM DEPARTMENT order by id) AS total",
-                wrapper
-                        .makeCountSql("SELECT * FROM DEPARTMENT order by id"));
+                wrapper.makeCountSql("SELECT * FROM DEPARTMENT order by id"));
     }
 
     private Object[] createNormalArgs() {
         return new Object[] {};
     }
+
 }
