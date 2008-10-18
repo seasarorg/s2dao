067751a56bc84e901f830f70a9b7b28679e1a28d
nakamura-to <toshihiro.nakamura@gmail.com>
2008-10-18 14:10:43 +0000

    [DAO-146]byte配列やSerializable型をPostgresSQLのOID型やBYTEA型にマッピングできるようにしました。
    git-svn-id: https://www.seasar.org/svn/s2dao/branches/s2dao-1.0.x@1271 a0133a49-3802-0410-849b-833ed23e894f

--- s2-dao/src/main/java/org/seasar/dao/dbms/PostgreSQL.java
+++ s2-dao/src/main/java/org/seasar/dao/dbms/PostgreSQL.java
@@ -15,11 +15,26 @@
  */
 package org.seasar.dao.dbms;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Blob;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.seasar.dao.types.BytesType;
+import org.seasar.dao.types.BytesType.Trait;
+
 /**
  * @author higa
  * 
  */
 public class PostgreSQL extends Standard {
+
+    public static Trait POSTGRE_TRAIT = new PostgreTrait();
 
     public String getSuffix() {
         return "_postgre";
@@ -27,5 +42,106 @@ public class PostgreSQL extends Standard {
 
     public String getSequenceNextValString(String sequenceName) {
         return "select nextval ('" + sequenceName + "')";
+    }
+
+    public static class PostgreTrait implements Trait {
+
+        public int getSqlType() {
+            return Types.BLOB;
+        }
+
+        public void set(final PreparedStatement ps, final int parameterIndex,
+                final byte[] bytes) throws SQLException {
+            ps.setBlob(parameterIndex, new BlobImpl(bytes));
+        }
+
+        public void set(final CallableStatement cs, final String parameterName,
+                final byte[] bytes) throws SQLException {
+            cs.setBytes(parameterName, bytes);
+        }
+
+        public byte[] get(final ResultSet rs, final int columnIndex)
+                throws SQLException {
+            return BytesType.toBytes(rs.getBlob(columnIndex));
+        }
+
+        public byte[] get(final ResultSet rs, final String columnName)
+                throws SQLException {
+            return BytesType.toBytes(rs.getBlob(columnName));
+        }
+
+        public byte[] get(final CallableStatement cs, final int columnIndex)
+                throws SQLException {
+            return BytesType.toBytes(cs.getBlob(columnIndex));
+        }
+
+        public byte[] get(final CallableStatement cs, final String columnName)
+                throws SQLException {
+            return BytesType.toBytes(cs.getBlob(columnName));
+        }
+
+    }
+
+    public static class BlobImpl implements Blob {
+
+        /** バイト列 */
+        protected byte[] bytes;
+
+        /**
+         * インスタンスを構築します。
+         * 
+         * @param bytes
+         *            バイト列
+         */
+        public BlobImpl(final byte[] bytes) {
+            this.bytes = bytes;
+        }
+
+        public InputStream getBinaryStream() throws SQLException {
+            return new ByteArrayInputStream(bytes);
+        }
+
+        public byte[] getBytes(final long pos, final int length)
+                throws SQLException {
+            if (length == bytes.length) {
+                return bytes;
+            }
+            final byte[] result = new byte[length];
+            System.arraycopy(bytes, 0, result, 0, length);
+            return result;
+        }
+
+        public long length() throws SQLException {
+            return bytes.length;
+        }
+
+        public long position(final Blob pattern, final long start)
+                throws SQLException {
+            throw new UnsupportedOperationException("position");
+        }
+
+        public long position(final byte[] pattern, final long start)
+                throws SQLException {
+            throw new UnsupportedOperationException("position");
+        }
+
+        public OutputStream setBinaryStream(final long pos) throws SQLException {
+            throw new UnsupportedOperationException("setBinaryStream");
+        }
+
+        public int setBytes(final long pos, final byte[] bytes,
+                final int offset, final int len) throws SQLException {
+            throw new UnsupportedOperationException("setBytes");
+        }
+
+        public int setBytes(final long pos, final byte[] bytes)
+                throws SQLException {
+            throw new UnsupportedOperationException("setBytes");
+        }
+
+        public void truncate(final long len) throws SQLException {
+            throw new UnsupportedOperationException("truncate");
+        }
+
     }
 }
---
+++ s2-dao/src/main/java/org/seasar/dao/types/ValueTypes.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2004-2008 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.dao.types;
+
+import org.seasar.dao.dbms.PostgreSQL;
+import org.seasar.extension.jdbc.ValueType;
+import org.seasar.extension.jdbc.types.StringClobType;
+
+/**
+ * 値タイプの定数です。
+ * 
+ * @author taedium
+ */
+public class ValueTypes {
+
+    /**
+     * CLOB用の値タイプです。
+     */
+    public final static ValueType CLOB = new StringClobType();
+
+    /**
+     * バイト配列用の値タイプです。
+     */
+    public final static ValueType BYTE_ARRAY = new BytesType(
+            BytesType.BYTES_TRAIT);
+
+    /**
+     * BLOB用の値タイプです。
+     */
+    public final static ValueType BLOB = new BytesType(BytesType.BLOB_TRAIT);
+
+    /**
+     * PostgreSQLのBLOB用の値タイプです。
+     */
+    public final static ValueType POSTGRE_BLOB = new BytesType(
+            PostgreSQL.POSTGRE_TRAIT);
+
+    /**
+     * オブジェクトをシリアライズしたバイト配列用の値タイプです。
+     */
+    public final static ValueType SERIALIZABLE_BYTE_ARRAY = new SerializableType(
+            BytesType.BYTES_TRAIT);
+
+    /**
+     * オブジェクトをシリアライズしたBLOB用の値タイプです。
+     */
+    public final static ValueType SERIALIZABLE_BLOB = new SerializableType(
+            BytesType.BLOB_TRAIT);
+
+    /**
+     * オブジェクトをシリアライズしたPostgreSQLのBLOB用の値タイプです。
+     */
+    public final static ValueType POSTGRE_SERIALIZABLE_BLOB = new SerializableType(
+            PostgreSQL.POSTGRE_TRAIT);
+}
